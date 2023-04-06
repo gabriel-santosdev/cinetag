08b6ced0c99334974d22d5cd05b7fb4f3277de80
gabriel-santosdev <gabrieljorgesilvasantos@outlook.com>
2023-04-05 21:13:35 -0300

    add favorite function

--- src/routes.js
+++ src/routes.js
@@ -4,16 +4,19 @@ import Inicio from "./pages/inicio";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "components/Footer";
 import Container from "components/Container";
+import FavoritosProvider from "contextos/Favoritos";
 
 function AppRoutes(){
     return (
         <BrowserRouter>
         <Cabecalho/>
             <Container>
+            <FavoritosProvider>
             <Routes>
                 <Route path="/" element={<Inicio />}></Route>
                 <Route path="/favoritos" element={<Favoritos/>}></Route>
             </Routes>
+            </FavoritosProvider>
             </Container>
         <Footer/>
         </BrowserRouter>
